4a44d8de06ae0c0533bdd8e05f94913fb3c9cab3
Alexandra <gosteli.alexandra@gmail.com>
2023-05-29 18:14:02 +0200

    added in main App some of new products from class Hotdrink

--- sem1/VendingMachine/src/App.java
+++ sem1/VendingMachine/src/App.java
@@ -1,4 +1,5 @@
 import Domen.Bottle;
+import Domen.HotDrink;
 import Domen.Product;
 import VendingMachines.VendingMachine;
 
@@ -7,13 +8,16 @@ public class App {
         Product item1 = new Product(1, "Lays", "Чипсы", 59);
         item1.setPrice(98);
         Product item2 = new Bottle(2, "Cola", "Water", 70, 500);
-
+        Product item5 = new HotDrink(5, "Coffee", "Hot Drinks", 100, 90);
+        Product item6 = new HotDrink(6, "Tee", "Hot Drinks", 100, 90);
         VendingMachine iMachine = new VendingMachine(300);
         iMachine.addProduct(item1);
         iMachine.addProduct(item2);
+        iMachine.addProduct(item5);
+        iMachine.addProduct(item6);
         iMachine.addProduct(new Product(3, "Масло", "Масло", 59));
         iMachine.addProduct(new Bottle(4, "Water", "Water", 170, 1500));
-
+        iMachine.addProduct(new HotDrink(7, "HotChoccolate", "Hot Drinks", 110, 89));
         for (Product prod : iMachine.getProducts()) {
             System.out.println(prod.toString());
         }
